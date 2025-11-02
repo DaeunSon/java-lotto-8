011ad1c6600191ea05079b022048d9a03532c49b
DaeunSon <130820902+DaeunSon@users.noreply.github.com>
2025-11-02 13:38:15 +0900

    feat : 로또 발매기 기능 구현
    1-1. 로또 구입 금액 입력받기
    1-2. 로또 당첨 번호 입력받기
    1-3. 보너스 번호 입력받기

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,46 @@
 package lotto;
 
+import lotto.domain.lotto.Lotto;
+import lotto.domain.lotto.WinningLotto;
+import lotto.domain.lottomachine.LottoMachine;
+import lotto.domain.lottoresult.LottoResult;
+import lotto.domain.lottoresult.LottoResultCalculator;
+import lotto.domain.money.Money;
+
+import static lotto.view.InputView.*;
+import static lotto.view.OutputView.*;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        //구입금액 입력받기
+        try{
+            Money money = new Money(inputMoney());
+            LottoMachine lottoMachine = new LottoMachine(money);
+
+            printPurchasedLottoCount(lottoMachine.countLottoTicket());
+
+            //구입한 로또 출력하기
+            lottoMachine.printPurchasedLottos();
+
+            //당첨 번호 입력받기
+            Lotto lotto = new Lotto(inputWinningNumbers());
+
+            //보너스 번호 입력받기
+            int bonusNumber = inputBonusNumber();
+
+            //당첨 통계 출력하기
+            printWinningStatisticsHeader();
+
+            //당첨 로또 생성하기
+            WinningLotto winningLotto = new WinningLotto(lotto, bonusNumber);
+
+            LottoResultCalculator calculator = new LottoResultCalculator(lottoMachine.getPurchasedLottos(), winningLotto);
+            LottoResult result = new LottoResult(calculator, money);
+            result.printLottoResults();
+        }
+        catch(IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            return;
+        }
     }
 }
